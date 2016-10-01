a0b90a3d1bb60d90d2446fcddecaa13067e7f3f0
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2016-10-01 15:31:22 +0200

    UP

--- app/js/sound.js
+++ app/js/sound.js
@@ -1,23 +1,14 @@
 const storage = require('electron-json-storage');
+window.onload = function () {
+  console.log("fisk");
+  var s_Noice = "TheNoise";
+  createjs.Sound.registerSound("./sound/TheNOOoicse.wav",s_Noice);
+
+  if (error) throw error;
+
+  console.log(data);
+};
 
 function playSound(Id){
   createjs.Sound.play(Id);
 }
-
-function addSound(path, Id) {
-  createjs.Sound.registerSound(path,id);
-}
-
-//Json sound loader
-var sounds = JSON.parse("sounds.json");
-
-window.onload = function () {
-  for (i in sounds) {
-    addSound(sounds[i].path, sounds[i].id);
-    console.log(i);
-  }
-};
-
-
-var s_Noice = "TheNoise";
-createjs.Sound.registerSound("./sound/TheNOOoicse.wav",s_Noice);
