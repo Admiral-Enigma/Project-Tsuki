f873caeff0fd28873585f5b4724cc0d12d8b6a50
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2016-10-01 13:49:10 +0200

    Started work on json loading

--- app/js/sound.js
+++ app/js/sound.js
@@ -1,8 +1,23 @@
-window.onload = function () {
-  var s_Noice = "TheNoise";
-  createjs.Sound.registerSound("./sound/TheNOOoicse.wav",s_Noice);
-};
+const storage = require('electron-json-storage');
 
 function playSound(Id){
   createjs.Sound.play(Id);
 }
+
+function addSound(path, Id) {
+  createjs.Sound.registerSound(path,id);
+}
+
+//Json sound loader
+var sounds = JSON.parse("sounds.json");
+
+window.onload = function () {
+  for (i in sounds) {
+    addSound(sounds[i].path, sounds[i].id);
+    console.log(i);
+  }
+};
+
+
+var s_Noice = "TheNoise";
+createjs.Sound.registerSound("./sound/TheNOOoicse.wav",s_Noice);
