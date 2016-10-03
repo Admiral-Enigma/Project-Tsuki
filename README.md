39c9851878620133487ccb9c045b398616fd13a1
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2016-10-03 19:35:29 +0200

    Made it easy to add new sounds ! :dancer:

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Project-Tsuki
 <img src="https://github.com/DemSquirrel/Project-Tsuki/blob/master/app/assets/icon.png" height="90">
 
-Hackable soundboard 
+Hackable soundboard
 
 # TODO list
-1. Make it easier for the user to add new a sound
+1. Make it easier for the user to add new a sound DONE!
 2. Enable the user to add hotkeys to each sound
 3. Make it more hackable!!
 4. Fix the top menu
--- app/js/sound.js
+++ app/js/sound.js
@@ -1,35 +1,45 @@
 var fs = require('fs');
+var jsonfile = require('jsonfile');
 const {shell} = require('electron')
-var sounds = JSON.parse(fs.readFileSync('./app/js/sounds.json', 'utf8'));
-
+var soundsFile = fs.readFileSync('./app/js/sounds.json', 'utf8');
+var sounds = JSON.parse(soundsFile);
+var json = "./app/js/sounds.json";
 
 function playSound(Id){
   createjs.Sound.play(Id);
 }
-try {
-  for(var i = 0; i < sounds.sounds.length; i++){
-    //registerSound(sounds.sounds[i].path, sounds.sounds[i].id);
-    createjs.Sound.registerSound(sounds.sounds[i].path, sounds.sounds[i].id);
-
-    //var id = sounds.sounds[i].id;
-    /**var btn = document.createElement('button');
-    btn.innerHTML = sounds.sounds[i].id;
-    btn.class = "pure-button pure-button-primary";
-    btn.onclick = playSound(sounds.sounds[i].id);**/
-
-    /**var button= document.createElement('button');
-    button.type = 'button'
-    button.name = sounds.sounds[i].id;
-    button.value = sounds.sounds[i].id;
-    button.class = "pure-button pure-button-primary";
-    button.onclick = function(){
-      createjs.Sound.play(sounds.sounds[i].id);
-    };
-    document.body.appendChild(button);**/
-    addButton(sounds.sounds[i].id);
-    console.log(sounds.sounds[i].path, sounds.sounds[i].id);
-    console.log("YES");
-  }
-} catch (e) {
-  console.log(e);
+function loadSoundToJson(id, path){
+  var data = {id:id, path:path};
+  sounds.sounds.push(data);
+  var soundsJSON = JSON.stringify(sounds);
+  fs.writeFileSync(json, soundsJSON);
 }
+jsonfile.readFile(json, function(err, obj) {
+  try {
+    for(var i = 0; i < obj.sounds.length; i++){
+      //registerSound(sounds.sounds[i].path, sounds.sounds[i].id);
+      createjs.Sound.registerSound(obj.sounds[i].path, obj.sounds[i].id);
+
+      //var id = sounds.sounds[i].id;
+      /**var btn = document.createElement('button');
+      btn.innerHTML = sounds.sounds[i].id;
+      btn.class = "pure-button pure-button-primary";
+      btn.onclick = playSound(sounds.sounds[i].id);**/
+
+      /**var button= document.createElement('button');
+      button.type = 'button'
+      button.name = sounds.sounds[i].id;
+      button.value = sounds.sounds[i].id;
+      button.class = "pure-button pure-button-primary";
+      button.onclick = function(){
+        createjs.Sound.play(sounds.sounds[i].id);
+      };
+      document.body.appendChild(button);**/
+      addButton(obj.sounds[i].id);
+      console.log(obj.sounds[i].path, obj.sounds[i].id);
+      console.log("YES");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
